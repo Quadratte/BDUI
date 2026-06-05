b4e56e30e5105cd9d9a2bed781369a471089cbf3
Sergei Philippov <quadratte@gmail.com>
2026-06-05 05:57:07 +0300

    Setup files and folders structure

---
+++ BDUI/SwiftUIView/CourseView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct CourseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseView_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseView()
+    }
+}
---
+++ BDUI/SwiftUIView/PlaceholderImageView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct PlaceholderImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaceholderImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaceholderImageView()
+    }
+}
