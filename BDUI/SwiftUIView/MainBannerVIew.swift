0c60963fe661517ea429ef0f4982e23492c172ea
Sergei Philippov <quadratte@gmail.com>
2026-06-08 23:49:42 +0300

    Added main banner view

--- BDUI/SwiftUIView/MainBannerVIew.swift
+++ BDUI/SwiftUIView/MainBannerVIew.swift
@@ -1,13 +1,46 @@
 import SwiftUI
 
 struct MainBannerVIew: View {
+    
+    let imageLink: String
+    let title: String
+    let captopn: String
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(alignment: .leading) {
+                AsyncImage(url: URL(string: imageLink)) { image
+                    in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(height: 160)
+                        .clipped()
+                } placeholder: {
+                    PlaceholderImageView()
+                        .frame(height: 160)
+                }
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(title)
+                        .font(.system(size: 24, weight: .bold, design: .serif))
+                    Text(captopn)
+                        .font(.system(size: 12))
+                        .fixedSize(horizontal: false, vertical: true)
+                        .lineLimit(2)
+                }
+                .padding(.leading, 20)
+            }
+        }
     }
 }
 
 struct MainBannerVIew_Previews: PreviewProvider {
     static var previews: some View {
-        MainBannerVIew()
+        MainBannerVIew(
+            imageLink: "https://images.unsplash.com/photo-1547658719-da2b51169166?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            title: "Title",
+            captopn: "Caption")
     }
 }
--- BDUI/SwiftUIView/PlaceholderImageView.swift
+++ BDUI/SwiftUIView/PlaceholderImageView.swift
@@ -2,7 +2,9 @@ import SwiftUI
 
 struct PlaceholderImageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Rectangle()
+            .foregroundColor(Color.gray .opacity(0.3)) 
+            
     }
 }
 
