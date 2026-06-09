667fef1bc4f5f97d961bffccc1adc9b0e35338e4
Sergei Philippov <quadratte@gmail.com>
2026-06-09 08:32:34 +0300

    Added main banner collection view cell

--- BDUI/SwiftUIView/MainBannerVIew.swift
+++ BDUI/SwiftUIView/MainBannerVIew.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-struct MainBannerVIew: View {
+struct MainBannerView: View {
     
     let imageLink: String
     let title: String
@@ -38,7 +38,7 @@ struct MainBannerVIew: View {
 
 struct MainBannerVIew_Previews: PreviewProvider {
     static var previews: some View {
-        MainBannerVIew(
+        MainBannerView(
             imageLink: "https://images.unsplash.com/photo-1547658719-da2b51169166?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             title: "Title",
             captopn: "Caption")
--- BDUI/View/MainBannerCollectionViewCell.swift
+++ BDUI/View/MainBannerCollectionViewCell.swift
@@ -1,1 +1,27 @@
-import Foundation
+import SwiftUI
+
+final class MainBannerCollectionViewCell: UICollectionViewCell {
+    
+    private var hostingController: UIHostingController<MainBannerView>!
+    
+    func configure(
+        imageLink: String,
+        title: String,
+        caption: String
+    ) {
+        guard hostingController == nil else { return }
+        let mainBannerView = MainBannerView(
+            imageLink: imageLink,
+            title: title,
+            captopn: caption)
+        
+        hostingController = UIHostingController(rootView: mainBannerView)
+        guard let hostingController = hostingController else { return }
+        
+        addSubview(hostingController.view)
+        hostingController.view.clipsToBounds = true
+        
+        hostingController.view.frame = superview?.bounds ?? CGRect(x: 0, y: 0, width: 0, height: 0)
+        
+    }
+}
